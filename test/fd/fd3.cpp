17f7948921983a6ce733e43f45cde10a2e066736
Mike Usenko <mmaxs@hotmail.ru>
2015-09-22 22:58:20 +0300

    add fd::close_all() test

--- test/fd/fd3.cpp
+++ test/fd/fd3.cpp
@@ -1,25 +1,65 @@
 
 #include "fd.h"
+#include "popen2.h"
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
+
+
+void proc_fd(std::string &_p)
+{
+  _p.clear();
+
+  int n = -1;
+  if ((n = snprintf(nullptr, 0, "/proc/%d/fd", ::getpid())) > 0)
+  {
+    _p.assign(n, 0);
+    snprintf(const_cast< char* >(_p.c_str()), n+1, "/proc/%d/fd", getpid());
+  };
+}
+
+
+void ls(const int _out, const char* const  _p)
+{
+  if (!_p)  return;
+
+  const char* cmd[] = { "ls",  "-AFl", "--time-style=long-iso", "--color=tty", _p, 0 };
+  popen2io(cmd[0], cmd, 0, _out);
+}
 
 
 
 int main(int _argc, char* const _argv[])
 {
-  fd err = 2;
-  printf("err [%d](%u): F_GETFD(0x%X) F_GETFL(0x%X)\n", (int)err, (bool)err, fcntl(err, F_GETFD), fcntl(err, F_GETFL));
-  err.setfd(FD_CLOEXEC);
-  printf("err [%d](%u): F_GETFD(0x%X) F_GETFL(0x%X)\n", (int)err, (bool)err, fcntl(err, F_GETFD), fcntl(err, F_GETFL));
+  std::string p;
+  proc_fd(p);
+  if (p.empty())
+  {
+    perror("proc_fd()");
+    return 1;
+  };
 
-  fd out = 1;
-  printf("out [%d](%u): F_GETFD(0x%X) F_GETFL(0x%X)\n", (int)out, (bool)out, fcntl(out, F_GETFD), fcntl(out, F_GETFL));
+  pid_t cpid;
+  int rd, wr;
 
-  err.dup2(out);
-  printf("out [%d](%u): F_GETFD(0x%X) F_GETFL(0x%X)\n", (int)out, (bool)out, fcntl(out, F_GETFD), fcntl(out, F_GETFL));
-  
-  out.dup2(err);
-  printf("err [%d](%u): F_GETFD(0x%X) F_GETFL(0x%X)\n", (int)err, (bool)err, fcntl(err, F_GETFD), fcntl(err, F_GETFL));
+  const char* args[] = { "cat", 0 };
+  if ((cpid = popen2(args[0], args,  &rd, &wr)) == -1)
+  {
+    perror("popen2()");
+    return 2;
+  };
+
+  fprintf(stderr, "pid=[%i] cpid=[%i] rd=[%i] wr=[%i]\n", getpid(), cpid, rd, wr);
+  ls(fileno(stderr), p.c_str());
+  getchar();
+
+  int x[] = { 0, 2, rd, -1 };
+  fd::close_all(x);
+
+  ls(fileno(stderr), p.c_str());
+  getchar();
+
 
   return 0;
 }
